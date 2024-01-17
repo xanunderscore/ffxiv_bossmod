b608d9ed3b7030820f19b48069f9e8f835739a4e
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-01-16 19:41:42 -0500

    implement 5% of the statuses

--- BossMod/Autorotation/BLU/BLURotation.cs
+++ BossMod/Autorotation/BLU/BLURotation.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace BossMod.BLU
@@ -7,7 +8,25 @@ namespace BossMod.BLU
         public class State(float[] cooldowns) : CommonRotation.PlayerState(cooldowns)
         {
             public bool TargetMortalFlame;
-            public float TargetBoMLeft;
+            public float TargetBoMLeft; // 60s max
+            public float TargetDropsyLeft;
+            public float TargetSlowLeft;
+            public float TargetBindLeft;
+            public float TargetLightheadedLeft;
+            public float TargetBegrimedLeft;
+
+            public (float Left, int Stacks) SurpanakhasFury; // 3/3 max
+            public int ReproStacks;
+
+            public float WaxingLeft; // 15s max
+            public float HarmonizedLeft; // 30s max
+            public float TinglingLeft; // 15s max
+            public float BoostLeft; // 30s max
+            public float BrushWithDeathLeft;
+            public float ToadOilLeft;
+            public float VeilLeft;
+            public float ApokalypsisLeft;
+            public float FlurryLeft;
 
             public AID[] BLUSlots = new AID[24];
 
@@ -28,11 +47,33 @@ namespace BossMod.BLU
                 return AID.MortalFlame;
 
             if (
+                state.TargetBoMLeft <= state.GCD + 2.22
+                && state.OnSlot(AID.BreathOfMagic)
+                && state.OnSlot(AID.Bristle)
+                && state.RangeToTarget <= 10
+                && state.BoostLeft == 0
+            )
+                return AID.Bristle;
+
+            if (
                 state.TargetBoMLeft <= state.GCD
                 && state.OnSlot(AID.BreathOfMagic)
                 && state.RangeToTarget <= 10
             )
                 return AID.BreathOfMagic;
+
+            if (state.OnSlot(AID.TripleTrident) && state.RangeToTarget < 3)
+            {
+                if (
+                    state.OnSlot(AID.Whistle)
+                    && state.CD(CDGroup.TripleTrident) - 2.22 <= state.GCD
+                    && state.HarmonizedLeft - 2.22 <= state.GCD
+                )
+                    return AID.Whistle;
+
+                if (state.CD(CDGroup.TripleTrident) <= state.GCD)
+                    return AID.TripleTrident;
+            }
 
             return AID.SonicBoom;
         }
