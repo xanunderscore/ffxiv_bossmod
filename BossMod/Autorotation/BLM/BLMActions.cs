c0db9caf95ae05522268c2adbd3c3740b7c08d05
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-02-01 18:17:23 -0500

    umbral soul > transpose

--- BossMod/Autorotation/BLM/BLMActions.cs
+++ BossMod/Autorotation/BLM/BLMActions.cs
@@ -81,6 +81,7 @@ namespace BossMod.BLM
                     ActionID.MakeSpell(AID.Transpose),
                     Player,
                     !Player.InCombat
+                        && !_state.Unlocked(AID.UmbralSoul)
                         && (
                             _state.ElementalLevel > 0 && _state.CurMP < 10000
                             || _state.ElementalLevel < 0
