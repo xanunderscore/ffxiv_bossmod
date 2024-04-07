bb54e1ea30d33bed3f04ec7818c9902ef49e38ec
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-04-07 18:35:16 -0400

    widen tracks

--- BossMod/Timeline/CooldownPlannerColumns.cs
+++ BossMod/Timeline/CooldownPlannerColumns.cs
@@ -1,4 +1,5 @@
-﻿using ImGuiNET;
+﻿using Dalamud.Interface.Utility;
+using ImGuiNET;
 using Newtonsoft.Json.Linq;
 
 namespace BossMod;
@@ -19,7 +20,7 @@ public class CooldownPlannerColumns : Timeline.ColumnGroup
     private ColumnPlannerTrackTarget _colTarget;
     private Dictionary<ActionID, int> _aidToColCooldown = new();
 
-    private float _trackWidth = 50;
+    private float _trackWidth = 50 * ImGuiHelpers.GlobalScale;
 
     public Class PlanClass => _plan.Class;
 
