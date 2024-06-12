b5f278ee6c20d608bacfeae4bb5633327d1031b1
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-06-12 19:45:21 -0400

    [pld] plan defs

--- BossMod/CooldownPlanner/PlanDefinitions.cs
+++ BossMod/CooldownPlanner/PlanDefinitions.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace BossMod;
 
 public static class PlanDefinitions
@@ -77,10 +74,20 @@ public static class PlanDefinitions
         var c = new ClassData(typeof(PLD.AID), PLD.Definitions.SupportedActions);
         c.CooldownTracks.Add(new("Sentinel", ActionID.MakeSpell(PLD.AID.Sentinel), 38));
         c.CooldownTracks.Add(new("Rampart", ActionID.MakeSpell(PLD.AID.Rampart), 8));
+        c.CooldownTracks.Add(new("Bulwark", ActionID.MakeSpell(PLD.AID.Bulwark), 52));
         c.CooldownTracks.Add(new("HallowedGround", ActionID.MakeSpell(PLD.AID.HallowedGround), 30));
         c.CooldownTracks.Add(new("Sheltron", ActionID.MakeSpell(PLD.AID.Sheltron), 35));
+        c.CooldownTracks.Add(new("Intervention", ActionID.MakeSpell(PLD.AID.Intervention), 62));
+        c.CooldownTracks.Add(new("Cover", ActionID.MakeSpell(PLD.AID.Cover), 45));
+        c.CooldownTracks.Add(new("Passage", ActionID.MakeSpell(PLD.AID.PassageOfArms), 70));
         c.CooldownTracks.Add(new("ArmsLength", ActionID.MakeSpell(PLD.AID.ArmsLength), 32));
         c.CooldownTracks.Add(new("Reprisal", ActionID.MakeSpell(PLD.AID.Reprisal), 22));
+        c.CooldownTracks.Add(new("Veil", ActionID.MakeSpell(PLD.AID.DivineVeil), 56));
+        c.CooldownTracks.Add(new("Taunt", ActionID.MakeSpell(DRK.AID.Provoke), 15));
+        c.CooldownTracks.Add(new("Shirk", ActionID.MakeSpell(DRK.AID.Shirk), 15));
+        c.CooldownTracks.Add(new("Sprint", CommonDefinitions.IDSprint, 1));
+        c.StrategyTracks.Add(new("FoF", typeof(CommonRotation.Strategy.OffensiveAbilityUse)));
+        c.StrategyTracks.Add(new("Requiescat", typeof(CommonRotation.Strategy.OffensiveAbilityUse)));
         return c;
     }
 
