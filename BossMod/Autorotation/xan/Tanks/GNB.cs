3e679428a68111b39de0dc535b0b9fe4763062dd
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-07-24 17:29:31 -0400

    shitty gnb that doesnt work

--- BossMod/Autorotation/xan/Tanks/GNB.cs
+++ BossMod/Autorotation/xan/Tanks/GNB.cs
@@ -17,12 +17,16 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : Basexan<A
 
     public int Ammo;
     public byte AmmoCombo;
+    public float Reign;
 
     public float SonicBreak;
     public bool Continuation;
     public float NoMercy;
 
     public int NumAOETargets;
+    public int NumReignTargets;
+
+    private Actor? BestReignTarget;
 
     public bool FastGCD => _state.AttackGCDTime <= 2.47f;
     public int MaxAmmo => Unlocked(TraitID.CartridgeChargeII) ? 3 : 2;
@@ -30,30 +34,35 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : Basexan<A
     private void CalcNextBestGCD(StrategyValues strategy, Actor? primaryTarget)
     {
         if (_state.CountdownRemaining > 0)
-        {
             return;
-        }
-
-        if (_state.CD(AID.GnashingFang) > 0 && NumAOETargets > 0 && Ammo >= 2 && _state.CD(AID.DoubleDown) < _state.GCD)
-            PushGCD(AID.DoubleDown, Player);
-
-        if (_state.CD(AID.GnashingFang) > 0 && SonicBreak > _state.GCD)
-            PushGCD(AID.SonicBreak, primaryTarget);
-
-        if (AmmoCombo == 2)
-        {
-            PushGCD(AID.WickedTalon, primaryTarget);
-            return;
-        }
-
-        if (AmmoCombo == 1)
-        {
-            PushGCD(AID.SavageClaw, primaryTarget);
-            return;
-        }
 
         if (_state.CD(AID.NoMercy) > 20 && Ammo > 0 && Unlocked(AID.GnashingFang) && _state.CD(AID.GnashingFang) < _state.GCD)
             PushGCD(AID.GnashingFang, primaryTarget);
+
+        if (NumAOETargets > 0 && Ammo >= 2 && _state.CD(AID.DoubleDown) < _state.GCD && NoMercy > _state.GCD)
+            PushGCD(AID.DoubleDown, Player);
+
+        if (SonicBreak > _state.GCD)
+            PushGCD(AID.SonicBreak, primaryTarget);
+
+        switch (AmmoCombo)
+        {
+            case 1:
+                PushGCD(AID.SavageClaw, primaryTarget);
+                return;
+            case 2:
+                PushGCD(AID.WickedTalon, primaryTarget);
+                return;
+            case 3:
+                PushGCD(AID.NobleBlood, BestReignTarget);
+                return;
+            case 4:
+                PushGCD(AID.LionHeart, BestReignTarget);
+                return;
+        }
+
+        if (Reign > _state.GCD && _state.CD(AID.GnashingFang) > 0 && _state.CD(AID.DoubleDown) > 0 && SonicBreak == 0)
+            PushGCD(AID.ReignOfBeasts, BestReignTarget);
 
         if (NumAOETargets > 1 && Unlocked(AID.DemonSlice))
         {
@@ -91,13 +100,14 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : Basexan<A
         }
     }
 
+    // TODO handle forced 2 cartridge burst
     private bool ShouldBust(StrategyValues strategy, AID spend)
     {
         if (!Unlocked(spend) || Ammo == 0)
             return false;
 
         if (NoMercy > _state.GCD)
-            return _state.CD(AID.DoubleDown) > NoMercy || Ammo == MaxAmmo;
+            return _state.CD(AID.DoubleDown) > NoMercy || Ammo == MaxAmmo || (Ammo == 1 && _state.CD(AID.DoubleDown) < NoMercy);
 
         return ComboLastMove is AID.BrutalShell or AID.DemonSlice && Ammo == MaxAmmo;
     }
@@ -110,6 +120,9 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : Basexan<A
         if (Continuation)
             PushOGCD(AID.Continuation, primaryTarget);
 
+        if (Unlocked(AID.Bloodfest) && _state.CanWeave(AID.Bloodfest, 0.6f, deadline) && Ammo == 0)
+            PushOGCD(AID.Bloodfest, primaryTarget);
+
         var usedNM = _state.CD(AID.NoMercy) > 20;
 
         if (ShouldNoMercy(strategy, deadline))
@@ -117,9 +130,6 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : Basexan<A
 
         if (usedNM)
         {
-            if (Unlocked(AID.Bloodfest) && _state.CanWeave(AID.Bloodfest, 0.6f, deadline) && Ammo == 0)
-                PushOGCD(AID.Bloodfest, primaryTarget);
-
             if (Unlocked(AID.BlastingZone) && _state.CanWeave(AID.BlastingZone, 0.6f, deadline))
                 PushOGCD(AID.BlastingZone, primaryTarget);
 
@@ -155,11 +165,13 @@ public sealed class GNB(RotationModuleManager manager, Actor player) : Basexan<A
         Ammo = gauge.Ammo;
         AmmoCombo = gauge.AmmoComboStep;
 
+        Reign = StatusLeft(SID.ReadyToReign);
         SonicBreak = StatusLeft(SID.ReadyToBreak);
         Continuation = Player.Statuses.Any(s => IsContinuationStatus((SID)s.ID));
         NoMercy = StatusLeft(SID.NoMercy);
 
         NumAOETargets = NumMeleeAOETargets(strategy);
+        (BestReignTarget, NumReignTargets) = SelectTarget(strategy, primaryTarget, 3, IsSplashTarget);
 
         CalcNextBestGCD(strategy, primaryTarget);
         QueueOGCD(deadline => CalcNextBestOGCD(strategy, primaryTarget, deadline));
