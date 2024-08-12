47b7993d4c27284575e4f47a39d292dc169dbe53
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-08-12 00:42:08 -0400

    prioritize gcds

--- BossMod/Autorotation/xan/Melee/MNK.cs
+++ BossMod/Autorotation/xan/Melee/MNK.cs
@@ -84,12 +84,49 @@ public sealed class MNK(RotationModuleManager manager, Actor player) : Attackxan
 
     public bool CanFormShift => Unlocked(AID.FormShift) && PerfectBalanceLeft == 0;
 
-    public const int AOEBreakpoint = 4;
+    // TODO incorporate crit calculation - rockbreaker is a gain on 3 at 22.1% crit
+    public int AOEBreakpoint => EffectiveForm == Form.OpoOpo ? 3 : 4;
     public bool UseAOE => NumAOETargets >= AOEBreakpoint;
 
-    private (Positional, bool) NextPositional => UseAOE
-        ? (Positional.Any, false)
-        : (CoeurlStacks > 0 ? Positional.Flank : Positional.Rear, EffectiveForm == Form.Coeurl);
+    private (Positional, bool) NextPositional
+    {
+        get
+        {
+            if (UseAOE)
+                return (Positional.Any, false);
+
+            var pos = CoeurlStacks > 0 ? Positional.Flank : Positional.Rear;
+            var imm = EffectiveForm == Form.Coeurl && NextGCD != AID.WindsReply;
+
+            return (pos, imm);
+        }
+    }
+
+    public enum GCDPriority
+    {
+        None = -1,
+        WindRanged = 100,
+        Basic = 300,
+        AOE = 400,
+        SSS = 500,
+        FiresReply = 700,
+        WindsReply = 800,
+        Blitz = 900,
+    }
+
+    // some monk OGCDs will be queued with higher prio than what user presses manually - the rotation is very drift-sensitive and monk has much less time to weave than other classes do
+    public enum OGCDPriority
+    {
+        None = -1,
+        TrueNorth = 100,
+        TFC = 150,
+        Potion = 200,
+        RiddleOfWind = 300,
+        ManualOGCD = 1901, // included for reference, not used here - actual value is 1901 + Low (2000) + 100 (in base class) = 4001
+        RiddleOfFire = 1910,
+        Brotherhood = 1915,
+        PerfectBalance = 1920
+    }
 
     public override void Exec(StrategyValues strategy, Actor? primaryTarget)
     {
@@ -157,52 +194,42 @@ public sealed class MNK(RotationModuleManager manager, Actor player) : Attackxan
         }
 
         if (NumBlitzTargets > 0)
-            PushGCD(currentBlitz, BestBlitzTarget);
+            PushGCD(currentBlitz, BestBlitzTarget, GCDPriority.Blitz);
 
-        // demo opener might be optimal sometimes
-        //if (FormShiftLeft > _state.GCD && CoeurlStacks == 0)
-        //    PushGCD(AID.Demolish, primaryTarget);
+        if (FiresReplyLeft > GCD && CurrentForm == Form.Raptor)
+            PushGCD(AID.FiresReply, BestRangedTarget, GCDPriority.FiresReply);
 
-        if (PerfectBalanceLeft == 0 && BlitzLeft == 0)
-        {
-            if (FormShiftLeft == 0 && FiresReplyLeft > GCD)
-                PushGCD(AID.FiresReply, BestRangedTarget);
+        WindsReply();
 
-            if (WindsReplyLeft > GCD)
-                PushGCD(AID.WindsReply, BestLineTarget);
-        }
-
-        if (UseAOE && Unlocked(AID.ArmOfTheDestroyer))
+        if (UseAOE)
         {
             if (EffectiveForm == Form.Coeurl)
-                PushGCD(AID.Rockbreaker, Player);
+                PushGCD(AID.Rockbreaker, Player, GCDPriority.AOE);
 
             if (EffectiveForm == Form.Raptor)
-                PushGCD(AID.FourPointFury, Player);
+                PushGCD(AID.FourPointFury, Player, GCDPriority.AOE);
 
-            PushGCD(AID.ArmOfTheDestroyer, Player);
+            PushGCD(AID.ArmOfTheDestroyer, Player, GCDPriority.AOE);
         }
-        else
+
+        switch (EffectiveForm)
         {
-            switch (EffectiveForm)
-            {
-                case Form.Coeurl:
-                    PushGCD(CoeurlStacks == 0 && Unlocked(AID.Demolish) ? AID.Demolish : AID.SnapPunch, primaryTarget); break;
-                case Form.Raptor:
-                    PushGCD(RaptorStacks == 0 && Unlocked(AID.TwinSnakes) ? AID.TwinSnakes : AID.TrueStrike, primaryTarget); break;
-                default:
-                    PushGCD(OpoStacks == 0 && Unlocked(AID.DragonKick) ? AID.DragonKick : AID.Bootshine, primaryTarget); break;
-            }
+            case Form.Coeurl:
+                PushGCD(CoeurlStacks == 0 && Unlocked(AID.Demolish) ? AID.Demolish : AID.SnapPunch, primaryTarget, GCDPriority.Basic); break;
+            case Form.Raptor:
+                PushGCD(RaptorStacks == 0 && Unlocked(AID.TwinSnakes) ? AID.TwinSnakes : AID.TrueStrike, primaryTarget, GCDPriority.Basic); break;
+            default:
+                PushGCD(OpoStacks == 0 && Unlocked(AID.DragonKick) ? AID.DragonKick : AID.Bootshine, primaryTarget, GCDPriority.Basic); break;
         }
 
         switch (strategy.Simple(Track.SSS))
         {
             case OffensiveStrategy.Force:
-                PushGCD(AID.SixSidedStar, primaryTarget, 500);
+                PushGCD(AID.SixSidedStar, primaryTarget, GCDPriority.SSS);
                 break;
             case OffensiveStrategy.Automatic:
                 if (!CanFitGCD(DowntimeIn - SSSApplicationDelay, 1))
-                    PushGCD(AID.SixSidedStar, primaryTarget, 500);
+                    PushGCD(AID.SixSidedStar, primaryTarget, GCDPriority.SSS);
                 break;
         }
     }
@@ -218,7 +245,7 @@ public sealed class MNK(RotationModuleManager manager, Actor player) : Attackxan
 
             // hack: allow double lunar opener - only in boss fights
             // trash packs should get regular lunar solar
-            var forceDoubleLunar = CombatTimer < 30 && !UseAOE;
+            var forceDoubleLunar = CombatTimer < 30 && NumAOETargets < 3;
             var forcedSolar = ForcedSolar || HasLunar && !HasSolar && !forceDoubleLunar;
 
             var canCoeurl = forcedSolar;
@@ -248,7 +275,7 @@ public sealed class MNK(RotationModuleManager manager, Actor player) : Attackxan
             return;
 
         if (CanWeave(AID.RiddleOfFire, 3) || CanFitGCD(FireLeft, 3))
-            PushAction(AID.PerfectBalance, Player, ActionQueue.Priority.ManualOGCD + 1, 0);
+            PushOGCD(AID.PerfectBalance, Player, OGCDPriority.PerfectBalance);
     }
 
     private void OGCD(StrategyValues strategy, Actor? primaryTarget)
@@ -257,36 +284,55 @@ public sealed class MNK(RotationModuleManager manager, Actor player) : Attackxan
             return;
 
         if (strategy.Option(Track.Potion).As<PotionStrategy>() == PotionStrategy.Now)
-            Hints.ActionsToExecute.Push(ActionDefinitions.IDPotionStr, Player, ActionQueue.Priority.ManualOGCD - 1);
+            Potion();
 
         if (strategy.BuffsOk())
         {
             if (strategy.Option(Track.Potion).As<PotionStrategy>() == PotionStrategy.PreBuffs && CanWeave(AID.Brotherhood, 4))
-                Hints.ActionsToExecute.Push(ActionDefinitions.IDPotionStr, Player, ActionQueue.Priority.ManualOGCD - 1, GCD - 0.9f);
+                Potion();
 
             QueuePB(strategy);
 
             if (CombatTimer >= 10 || BeastCount == 2)
-                PushAction(AID.Brotherhood, Player, ActionQueue.Priority.ManualOGCD + 3, 0);
+                PushOGCD(AID.Brotherhood, Player, OGCDPriority.Brotherhood);
 
             if (ShouldRoF)
-                PushAction(AID.RiddleOfFire, Player, ActionQueue.Priority.ManualOGCD + 2, GCD - 0.8f);
+                PushOGCD(AID.RiddleOfFire, Player, OGCDPriority.RiddleOfFire, GCD - EarliestRoF(AnimationLockDelay));
 
             if (CD(AID.RiddleOfFire) > 0)
-                PushOGCD(AID.RiddleOfWind, Player);
+                PushOGCD(AID.RiddleOfWind, Player, OGCDPriority.RiddleOfWind);
 
             if (NextPositionalImminent && !NextPositionalCorrect)
-                PushOGCD(AID.TrueNorth, Player, delay: ShouldRoF ? 0 : GCD - 0.8f);
+                PushOGCD(AID.TrueNorth, Player, OGCDPriority.TrueNorth, ShouldRoF ? 0 : GCD - 0.8f);
         }
 
         if (Chakra >= 5)
         {
             if (NumLineTargets >= 3)
-                PushOGCD(AID.HowlingFist, BestLineTarget);
+                PushOGCD(AID.HowlingFist, BestLineTarget, OGCDPriority.TFC);
 
-            PushOGCD(AID.SteelPeak, primaryTarget);
+            PushOGCD(AID.SteelPeak, primaryTarget, OGCDPriority.TFC);
         }
     }
+
+    private void WindsReply()
+    {
+        if (WindsReplyLeft <= GCD || PerfectBalanceLeft > GCD || BlitzLeft > GCD)
+            return;
+
+        var prio = GCDPriority.WindRanged;
+
+        // use early during buffs, or use now if about to expire
+        if (FireLeft > GCD || !CanFitGCD(WindsReplyLeft, 1))
+            prio = GCDPriority.WindsReply;
+
+        PushGCD(AID.WindsReply, BestLineTarget, prio);
+    }
+
+    private float DesiredFireWindow => GCDLength * 10;
+    private float EarliestRoF(float estimatedDelay) => MathF.Max(estimatedDelay + 0.6f, 20.6f - DesiredFireWindow);
+
+    private void Potion() => Hints.ActionsToExecute.Push(ActionDefinitions.IDPotionStr, Player, ActionQueue.Priority.Low + 100 + (float)OGCDPriority.Potion);
 
     private bool ShouldRoF => CanWeave(AID.RiddleOfFire) && !CanWeave(AID.Brotherhood);
 
