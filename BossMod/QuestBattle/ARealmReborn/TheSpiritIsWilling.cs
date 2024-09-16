63ce46147ba92cbee33259be9e5b90c7ba7819de
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-09-16 12:00:44 -0400

    thing

---
+++ BossMod/QuestBattle/ARealmReborn/TheSpiritIsWilling.cs
@@ -0,0 +1,18 @@
+﻿namespace BossMod.QuestBattle.ARealmReborn.TheSpiritIsWilling;
+
+[Quest(BossModuleInfo.Maturity.WIP, 319)]
+internal class Quest(WorldState ws) : QuestBattle(ws)
+{
+    public override List<QuestObjective> DefineObjectives(WorldState ws) => [
+        new QuestObjective(ws)
+            .Hints((player, hints) => {
+                hints.Center = new(-240, -6);
+                hints.Bounds = new ArenaBoundsSquare(15);
+
+                hints.AddForbiddenZone(new AOEShapeRect(4, 3, 4), new(-229, -2));
+
+                foreach(var h in hints.PotentialTargets)
+                    h.Priority = 0;
+            })
+        ];
+}
