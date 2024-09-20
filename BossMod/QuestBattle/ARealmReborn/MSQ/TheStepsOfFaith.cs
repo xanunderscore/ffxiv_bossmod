5d31fb1d5734bdda82c51b9e244e20fdb26c9705
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-09-19 20:33:23 -0400

    pointless

--- BossMod/QuestBattle/ARealmReborn/MSQ/TheStepsOfFaith.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BossMod.QuestBattle.ARealmReborn.MSQ;
-
-[Quest(BossModuleInfo.Maturity.Contributed, 885)]
-internal class TheStepsOfFaith(WorldState ws) : QuestBattle(ws)
-{
-    public override List<QuestObjective> DefineObjectives(WorldState ws) => [
-        new QuestObjective(ws).WithConnection(new Vector3(0, 0, 260))
-    ];
-}
