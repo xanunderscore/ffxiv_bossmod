465eac529704307b98e098fc0d4d60298784274a
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-09-22 16:20:51 -0400

    cleanup

--- BossMod/Modules/Endwalker/Quest/WorthyOfHisBack.cs
+++ BossMod/Modules/Endwalker/Quest/WorthyOfHisBack.cs
@@ -8,45 +8,56 @@ public enum OID : uint
 
 public enum AID : uint
 {
-    _AutoAttack_ = 28383, // Boss->player, no cast, single-target
-    _Spell_Kleos = 25597, // Boss->self, 3.0s cast, range 40 circle
-    _Ability_ = 25599, // Boss->location, no cast, single-target
-    _Weaponskill_CircumzenithalArc = 25598, // Boss->self, 5.0+2.2s cast, single-target
-    _Weaponskill_CircumzenithalArc1 = 28466, // 233C->self, 7.3s cast, range 40 180-degree cone
-    _Spell_TrueBlink = 25600, // Boss->self, 3.0s cast, single-target
-    _Ability_1 = 25601, // 342D->location, no cast, single-target
-    _Weaponskill_CrepuscularRay = 25595, // 342D->location, 5.0s cast, width 8 rect charge
-    _Weaponskill_CircumzenithalArc2 = 28376, // 233C->self, 7.3s cast, range 40 180-degree cone
-    _Spell_Enomotos = 25603, // 233C->self, 5.0s cast, range 6 circle
-    _Weaponskill_CircleOfBrilliance = 25602, // Boss->self, 5.0s cast, range 5 circle
-    _Spell_Enomotos1 = 25604, // 233C->self, no cast, range 6 circle
-    _Ability_MousasMantle = 25592, // Boss->self, 6.5s cast, single-target
-    _Weaponskill_EpeaPteroenta = 25605, // Boss->self, 7.0s cast, range 20 120-degree cone
-    _Weaponskill_EpeaPteroenta1 = 25606, // Boss->self, no cast, range 20 ?-degree cone
-    _Weaponskill_EpeaPteroenta2 = 25607, // 233C->self, 8.0s cast, range 20 120-degree cone
-    _Weaponskill_Parhelion = 25610, // 233C->self, 5.0s cast, range 20 45-degree cone
-    _Weaponskill_Parhelion2 = 25612, // 233C->self, 4.0s cast, range 10 circle
-    _Weaponskill_Parhelion3 = 25611, // 233C->self, 1.5s cast, range 20 45-degree cone
-    _Weaponskill_Parhelion4 = 25613, // 233C->self, 4.0s cast, range 10-15 donut
-    _Weaponskill_Parhelion5 = 25614, // 233C->self, 4.0s cast, range 15-20 donut
-    _AutoAttack_1 = 28384, // Boss->player, no cast, single-target
-    _Weaponskill_Parhelion1 = 25608, // Boss->self, 5.0s cast, single-target
-    _Ability_MagossMantle = 25593, // Boss->self, 6.5s cast, single-target
-    _Spell_TrueStone = 28385, // Boss->player, no cast, single-target
-    _Spell_TrueAeroIV = 25615, // Boss->self, 3.0s cast, range 40 circle
-    _Spell_Windage = 25616, // 342E->self, 2.5s cast, range 5 circle
-    _Spell_AfflatusAzem = 25617, // Boss->location, 4.0s cast, range 5 circle
-    _Weaponskill_Parhelion6 = 25609, // Boss->self, 5.0s cast, single-target
-    _Spell_AfflatusAzem1 = 25618, // Boss->location, 1.0s cast, range 5 circle
-    _Spell_TrueHoly = 25619, // Boss->self, 5.0s cast, range 40 circle
-    _Spell_Windage1 = 28116, // 342E->self, 9.0s cast, range 5 circle
-    _Spell_TrueStoneIV = 25620, // Boss->self, 3.0s cast, single-target
-    _Spell_TrueStoneIV1 = 25621, // 233C->location, 6.0s cast, range 10 circle
-    _Weaponskill_ = 25622, // Boss->self, no cast, single-target
-    _Spell_Enomotos2 = 28392, // 233C->location, 3.0s cast, range 4 circle
+    AutoAttack = 28383, // Boss->player, no cast, single-target
+    AutoAttackDancer = 28384, // Boss->player, no cast, single-target
+    TrueStone = 28385, // Boss->player, no cast, single-target, whm form autoattack
+    TrueBlink = 25600, // Boss->self, 3.0s cast, single-target, boss dash to some location
+
+    MousasMantle = 25592, // Boss->self, 6.5s cast, single-target, switch jobs to dancer
+    MagosMantle = 25593, // Boss->self, 6.5s cast, single-target, switch jobs to whm
+
+    Kleos = 25597, // Boss->self, 3.0s cast, range 40 circle, raidwide
+
+    CircumzenithalArcVisual = 25598, // Boss->self, 5.0+2.2s cast, single-target
+    CircumzenithalArcFirst = 28466, // 233C->self, 7.3s cast, range 40 180-degree cone
+    CircumzenithalArcSecond = 28376, // 233C->self, 7.3s cast, range 40 180-degree cone
+
+    CrepuscularRay = 25595, // 342D->location, 5.0s cast, width 8 rect charge
+
+    CircleOfBrilliance = 25602, // Boss->self, 5.0s cast, range 5 circle
+
+    EnomotosFirst = 25603, // 233C->self, 5.0s cast, range 6 circle
+    EnomotosRest = 25604, // 233C->self, no cast, range 6 circle
+
+    EnomotosSmall = 28392, // 233C->location, 3.0s cast, range 4 circle
+
+    EpeaPteroentaFirst = 25605, // Boss->self, 7.0s cast, range 20 120-degree cone
+    EpeaPteroentaSecond = 25607, // 233C->self, 8.0s cast, range 20 120-degree cone
+    EpeaPteroentaIDK = 25606, // Boss->self, no cast, range 20 ?-degree cone
+
+    ParhelionVisual = 25608, // Boss->self, 5.0s cast, single-target
+    ParhelionVisual2 = 25609, // Boss->self, 5.0s cast, single-target
+    ParhelionConeFirst = 25610, // 233C->self, 5.0s cast, range 20 45-degree cone
+    ParhelionConeRest = 25611, // 233C->self, 1.5s cast, range 20 45-degree cone
+
+    ParhelionDonut1 = 25612, // 233C->self, 4.0s cast, range 10 circle
+    ParhelionDonut2 = 25613, // 233C->self, 4.0s cast, range 10-15 donut
+    ParhelionDonut3 = 25614, // 233C->self, 4.0s cast, range 15-20 donut
+
+    TrueAeroIV = 25615, // Boss->self, 3.0s cast, range 40 circle
+    TrueHoly = 25619, // Boss->self, 5.0s cast, range 40 circle
+
+    AfflatusAzemFirst = 25617, // Boss->location, 4.0s cast, range 5 circle
+    AfflatusAzemChase = 25618, // Boss->location, 1.0s cast, range 5 circle
+
+    Windage = 25616, // 342E->self, 2.5s cast, range 5 circle
+    WindageSlow = 28116, // 342E->self, 9.0s cast, range 5 circle
+
+    TrueStoneIVVisual = 25620, // Boss->self, 3.0s cast, single-target
+    TrueStoneIV = 25621, // 233C->location, 6.0s cast, range 10 circle
 }
 
-class Kleos(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID._Spell_Kleos), "Raidwide + death wall spawn");
+class Kleos(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID.Kleos), "Raidwide + death wall spawn");
 
 class ParhelionCone(BossModule module) : Components.GenericRotatingAOE(module)
 {
@@ -68,13 +79,13 @@ class ParhelionCone(BossModule module) : Components.GenericRotatingAOE(module)
 
     public override void OnCastStarted(Actor caster, ActorCastInfo spell)
     {
-        if ((AID)spell.Action.ID == AID._Weaponskill_Parhelion)
+        if ((AID)spell.Action.ID == AID.ParhelionConeFirst)
             Sequences.Add(new(new AOEShapeCone(20, 22.5f.Degrees()), caster.Position, caster.Rotation, GetAngle(NextDirection), Module.CastFinishAt(spell), 2.6f, 9));
     }
 
     public override void OnEventCast(Actor caster, ActorCastEvent spell)
     {
-        if ((AID)spell.Action.ID is AID._Weaponskill_Parhelion or AID._Weaponskill_Parhelion3)
+        if ((AID)spell.Action.ID is AID.ParhelionConeFirst or AID.ParhelionConeRest)
             AdvanceSequence(caster.Position, caster.Rotation, WorldState.CurrentTime);
     }
 
@@ -89,11 +100,11 @@ class ParhelionCone(BossModule module) : Components.GenericRotatingAOE(module)
             Sequences[i] = Sequences[i] with { Increment = GetAngle(NextDirection) };
     }
 }
-class Parhelion(BossModule module) : Components.ConcentricAOEs(module, [new AOEShapeCircle(10), new AOEShapeDonut(10, 15), new AOEShapeDonut(15, 20)])
+class ParhelionDonut(BossModule module) : Components.ConcentricAOEs(module, [new AOEShapeCircle(10), new AOEShapeDonut(10, 15), new AOEShapeDonut(15, 20)])
 {
     public override void OnCastStarted(Actor caster, ActorCastInfo spell)
     {
-        if ((AID)spell.Action.ID == AID._Weaponskill_Parhelion2)
+        if ((AID)spell.Action.ID == AID.ParhelionDonut1)
             AddSequence(Module.Center, Module.CastFinishAt(spell));
     }
 
@@ -101,9 +112,9 @@ class Parhelion(BossModule module) : Components.ConcentricAOEs(module, [new AOES
     {
         var order = (AID)spell.Action.ID switch
         {
-            AID._Weaponskill_Parhelion2 => 0,
-            AID._Weaponskill_Parhelion4 => 1,
-            AID._Weaponskill_Parhelion5 => 2,
+            AID.ParhelionDonut1 => 0,
+            AID.ParhelionDonut2 => 1,
+            AID.ParhelionDonut3 => 2,
             _ => -1
         };
         if (!AdvanceSequence(order, caster.Position, WorldState.FutureTime(2f)))
@@ -127,30 +138,33 @@ class EpeaPteroenta(BossModule module) : Components.GenericAOEs(module)
 
     public override void OnCastStarted(Actor caster, ActorCastInfo spell)
     {
-        if ((AID)spell.Action.ID is AID._Weaponskill_EpeaPteroenta or AID._Weaponskill_EpeaPteroenta2)
+        if ((AID)spell.Action.ID is AID.EpeaPteroentaFirst or AID.EpeaPteroentaSecond)
             Casters.Add(caster);
     }
 
     public override void OnCastFinished(Actor caster, ActorCastInfo spell)
     {
-        if ((AID)spell.Action.ID is AID._Weaponskill_EpeaPteroenta or AID._Weaponskill_EpeaPteroenta2)
+        if ((AID)spell.Action.ID is AID.EpeaPteroentaFirst or AID.EpeaPteroentaSecond)
             Casters.Remove(caster);
     }
 }
 
-class CrepuscularRay(BossModule module) : Components.ChargeAOEs(module, ActionID.MakeSpell(AID._Weaponskill_CrepuscularRay), 4);
-class CircumzenithalArc(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_CircumzenithalArc1), new AOEShapeCone(40, 90.Degrees()));
-class CircumzenithalArc2(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_CircumzenithalArc2), new AOEShapeCone(40, 90.Degrees()))
+class CrepuscularRay(BossModule module) : Components.ChargeAOEs(module, ActionID.MakeSpell(AID.CrepuscularRay), 4);
+class CircumzenithalArc(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.CircumzenithalArcFirst), new AOEShapeCone(40, 90.Degrees()));
+class CircumzenithalArcSecond(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.CircumzenithalArcSecond), new AOEShapeCone(40, 90.Degrees()))
 {
+    private CrepuscularRay? ray;
     public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
     {
+        ray ??= Module.FindComponent<CrepuscularRay>();
         // skip forbidden zone creation if charges are still active, AI doesn't handle it well
-        if (Module.FindComponent<CrepuscularRay>()?.Casters.Count == 0)
+        // TODO does this work better with new pathfinder?
+        if (ray?.Casters.Count == 0)
             base.AddAIHints(slot, actor, assignment, hints);
     }
 }
-class CircleOfBrilliance(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_CircleOfBrilliance), new AOEShapeCircle(5));
-class Enomotos(BossModule module) : Components.Exaflare(module, new AOEShapeCircle(6), ActionID.MakeSpell(AID._Spell_Enomotos))
+class CircleOfBrilliance(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.CircleOfBrilliance), new AOEShapeCircle(5));
+class Enomotos(BossModule module) : Components.Exaflare(module, new AOEShapeCircle(6), ActionID.MakeSpell(AID.EnomotosFirst))
 {
     public override void OnCastStarted(Actor caster, ActorCastInfo spell)
     {
@@ -173,7 +187,7 @@ class Enomotos(BossModule module) : Components.Exaflare(module, new AOEShapeCirc
 
     public override void OnEventCast(Actor caster, ActorCastEvent spell)
     {
-        if ((AID)spell.Action.ID is AID._Spell_Enomotos or AID._Spell_Enomotos1)
+        if ((AID)spell.Action.ID is AID.EnomotosFirst or AID.EnomotosRest)
         {
             var line = Lines.FirstOrDefault(x => x.Next.AlmostEqual(caster.Position, 1));
             if (line != null)
@@ -182,7 +196,7 @@ class Enomotos(BossModule module) : Components.Exaflare(module, new AOEShapeCirc
     }
 }
 
-class DeathWall(BossModule module) : Components.GenericAOEs(module, ActionID.MakeSpell(AID._Spell_Kleos))
+class DeathWall(BossModule module) : Components.GenericAOEs(module, ActionID.MakeSpell(AID.Kleos))
 {
     private DateTime? ExpectedActivation;
     private readonly AOEShape Voidzone = new AOEShapeDonut(19.5f, 100);
@@ -211,10 +225,10 @@ class DeathWall(BossModule module) : Components.GenericAOEs(module, ActionID.Mak
     }
 }
 
-class Windage(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Spell_Windage), new AOEShapeCircle(5));
-class AfflatusAzem(BossModule module) : Components.StandardChasingAOEs(module, new AOEShapeCircle(5), ActionID.MakeSpell(AID._Spell_AfflatusAzem), ActionID.MakeSpell(AID._Spell_AfflatusAzem1), 5, 2.1f, 5);
-class WindageSlow(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Spell_Windage1), new AOEShapeCircle(5));
-class TrueHoly(BossModule module) : Components.KnockbackFromCastTarget(module, ActionID.MakeSpell(AID._Spell_TrueHoly), 20)
+class Windage(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.Windage), new AOEShapeCircle(5));
+class AfflatusAzem(BossModule module) : Components.StandardChasingAOEs(module, new AOEShapeCircle(5), ActionID.MakeSpell(AID.AfflatusAzemFirst), ActionID.MakeSpell(AID.AfflatusAzemChase), 5, 2.1f, 5);
+class WindageSlow(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID.WindageSlow), new AOEShapeCircle(5));
+class TrueHoly(BossModule module) : Components.KnockbackFromCastTarget(module, ActionID.MakeSpell(AID.TrueHoly), 20)
 {
     public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
     {
@@ -223,8 +237,17 @@ class TrueHoly(BossModule module) : Components.KnockbackFromCastTarget(module, A
             hints.ActionsToExecute.Push(action, actor, ActionQueue.Priority.Medium);
     }
 }
-class TrueStoneIV(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID._Spell_TrueStoneIV1), 10, maxCasts: 7);
-class Enomotos3(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID._Spell_Enomotos2), 4);
+class TrueStoneIV(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID.TrueStoneIV), 10, maxCasts: 7);
+class EnomotosSmall(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID.EnomotosSmall), 4);
+
+class Adds(BossModule module) : Components.Adds(module, 0x3864)
+{
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        foreach (var e in hints.PotentialTargets)
+            e.Priority = e.Actor.OID == 0x3864 ? 1 : 0;
+    }
+}
 
 public class VenatStates : StateMachineBuilder
 {
@@ -234,36 +257,22 @@ public class VenatStates : StateMachineBuilder
             .ActivateOnEnter<DeathWall>()
             .ActivateOnEnter<Kleos>()
             .ActivateOnEnter<CircumzenithalArc>()
-            .ActivateOnEnter<CircumzenithalArc2>()
+            .ActivateOnEnter<CircumzenithalArcSecond>()
             .ActivateOnEnter<CrepuscularRay>()
             .ActivateOnEnter<CircleOfBrilliance>()
             .ActivateOnEnter<Enomotos>()
             .ActivateOnEnter<EpeaPteroenta>()
-            .ActivateOnEnter<Parhelion>()
+            .ActivateOnEnter<ParhelionDonut>()
             .ActivateOnEnter<ParhelionCone>()
             .ActivateOnEnter<Windage>()
             .ActivateOnEnter<AfflatusAzem>()
             .ActivateOnEnter<WindageSlow>()
             .ActivateOnEnter<TrueHoly>()
             .ActivateOnEnter<TrueStoneIV>()
-            .ActivateOnEnter<Enomotos3>()
-            ;
+            .ActivateOnEnter<EnomotosSmall>()
+            .ActivateOnEnter<Adds>();
     }
 }
 
-[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.Quest, GroupID = 69968, NameID = 10586)]
-public class Venat(WorldState ws, Actor primary) : BossModule(ws, primary, new(-630, 72), new ArenaBoundsCircle(24.5f))
-{
-    protected override bool CheckPull() => true;
-
-    protected override void DrawArenaForeground(int pcSlot, Actor pc)
-    {
-        Arena.Actors(WorldState.Actors.Where(x => !x.IsAlly), ArenaColor.Enemy);
-    }
-
-    protected override void CalculateModuleAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
-    {
-        foreach (var e in hints.PotentialTargets)
-            e.Priority = e.Actor.OID == 0x3864 ? 1 : 0;
-    }
-}
+[ModuleInfo(BossModuleInfo.Maturity.Contributed, GroupType = BossModuleInfo.GroupType.Quest, GroupID = 69968, NameID = 10586)]
+public class Venat(WorldState ws, Actor primary) : BossModule(ws, primary, new(-630, 72), new ArenaBoundsCircle(24.5f));
