31467cde6956cb0776723306361d57725aa1ea8f
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-10-09 17:11:11 -0400

    heavens bussy

---
+++ BossMod/Modules/Endwalker/Quest/AsTheHeavensBurn/P1TerminusIdolizer.cs
@@ -0,0 +1,123 @@
+﻿using BossMod.QuestBattle.Endwalker.MSQ;
+
+namespace BossMod.Endwalker.Quest.AsTheHeavensBurn.P1TerminusIdolizer;
+
+public enum OID : uint
+{
+    Boss = 0x35E9,
+    Helper = 0x233C,
+    TerminusDetonator = 0x35E5, // R2.000, x0 (spawn during fight)
+}
+
+public enum AID : uint
+{
+    _AutoAttack_Attack = 872, // 375A/375B/3759/35E1/35E0/375C->35F2, no cast, single-target
+    _Weaponskill_DeadlyCharge = 26995, // Boss->location, 5.0s cast, width 10 rect charge
+    _AutoAttack_ = 26994, // Boss->35F5, no cast, single-target
+    _Weaponskill_GriefOfParting = 26996, // Boss->self, 5.0s cast, range 40 circle
+    _Weaponskill_DeadlyTentacles = 26997, // Boss->35F5, 5.0s cast, single-target
+    _Weaponskill_DeadlyTentacles1 = 26998, // Boss->35F5, no cast, single-target
+    _Weaponskill_TentacleWhipLFirst = 27004, // Boss->self, 5.0s cast, range 60 180-degree cone
+    _Weaponskill_TentacleWhip = 27005, // Boss->self, no cast, single-target
+    _Weaponskill_TentacleWhipRSecond = 27006, // Helper->self, 7.0s cast, range 60 180-degree cone
+    _Ability_Shout = 27000, // Boss->self, no cast, single-target
+    _Weaponskill_SelfDestruct = 26991, // TerminusDetonator->self, no cast, range 6 circle
+    _Weaponskill_Petrifaction = 26999, // Boss->self, 4.0s cast, range 60 circle
+    _Weaponskill_TentacleWhipRFirst = 27001, // Boss->self, 5.0s cast, range 60 180-degree cone
+    _Weaponskill_TentacleWhipLSecond = 27003, // Helper->self, 7.0s cast, range 60 180-degree cone
+    _Weaponskill_TentacleWhip1 = 27002, // Boss->self, no cast, single-target
+    _Weaponskill_Whack = 27007, // Boss->35F5, 5.0s cast, single-target
+    _Weaponskill_Whack1 = 27008, // Boss->35F5, no cast, single-target
+    _Weaponskill_Whack2 = 27009, // Boss->35F5, no cast, single-target
+}
+
+public enum IconID : uint
+{
+    Stack = 218, // 35F5
+}
+
+public enum TetherID : uint
+{
+    BombTether = 17, // 35E5->35F5
+}
+
+class DeadlyCharge(BossModule module) : Components.ChargeAOEs(module, ActionID.MakeSpell(AID._Weaponskill_DeadlyCharge), 5);
+class GriefOfParting(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID._Weaponskill_GriefOfParting));
+class DeadlyTentacles(BossModule module) : Components.SingleTargetCast(module, ActionID.MakeSpell(AID._Weaponskill_DeadlyTentacles));
+class TentacleWhipR1(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_TentacleWhipRFirst), new AOEShapeCone(60, 90.Degrees()));
+class TentacleWhipR2(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_TentacleWhipLSecond), new AOEShapeCone(60, 90.Degrees()))
+{
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        if (Module.FindComponent<TentacleWhipR1>()?.Casters.Count > 0)
+            yield break;
+        else
+            foreach (var h in base.ActiveAOEs(slot, actor))
+                yield return h;
+    }
+}
+class TentacleWhipL1(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_TentacleWhipLFirst), new AOEShapeCone(60, 90.Degrees()));
+class TentacleWhipL2(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_TentacleWhipRSecond), new AOEShapeCone(60, 90.Degrees()))
+{
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        if (Module.FindComponent<TentacleWhipL1>()?.Casters.Count > 0)
+            yield break;
+        else
+            foreach (var h in base.ActiveAOEs(slot, actor))
+                yield return h;
+    }
+}
+class SelfDestruct(BossModule module) : Components.GenericStackSpread(module)
+{
+    private readonly List<Actor> Bombs = [];
+
+    public override void OnTethered(Actor source, ActorTetherInfo tether)
+    {
+        if (tether.ID == (uint)TetherID.BombTether)
+        {
+            Bombs.Add(source);
+            if (Stacks.Count == 0)
+                Stacks.Add(new Stack(WorldState.Actors.Find(tether.Target)!, 3, activation: WorldState.FutureTime(9.1f)));
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if (spell.Action.ID == (uint)AID._Weaponskill_SelfDestruct)
+        {
+            Bombs.Remove(caster);
+            if (Bombs.Count == 0)
+                Stacks.Clear();
+        }
+    }
+}
+class Petrifaction(BossModule module) : Components.CastGaze(module, ActionID.MakeSpell(AID._Weaponskill_Petrifaction));
+class Whack(BossModule module) : Components.SingleTargetCast(module, ActionID.MakeSpell(AID._Weaponskill_Whack));
+
+class AutoAlphi(BossModule module) : Components.RotationModule<AlphinaudAI>(module);
+
+class TerminusIdolizerStates : StateMachineBuilder
+{
+    public TerminusIdolizerStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<DeadlyCharge>()
+            .ActivateOnEnter<GriefOfParting>()
+            .ActivateOnEnter<TentacleWhipL1>()
+            .ActivateOnEnter<TentacleWhipL2>()
+            .ActivateOnEnter<TentacleWhipR1>()
+            .ActivateOnEnter<TentacleWhipR2>()
+            .ActivateOnEnter<DeadlyTentacles>()
+            .ActivateOnEnter<SelfDestruct>()
+            .ActivateOnEnter<Petrifaction>()
+            .ActivateOnEnter<Whack>()
+            .ActivateOnEnter<AutoAlphi>();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 804, NameID = 10932)]
+public class TerminusIdolizer(WorldState ws, Actor primary) : BossModule(ws, primary, new(-300.75f, 151.5f), new ArenaBoundsCircle(19.5f))
+{
+    protected override bool CheckPull() => PrimaryActor.InCombat;
+}
---
+++ BossMod/Modules/Endwalker/Quest/AsTheHeavensBurn/P3TerminusLacerator.cs
@@ -0,0 +1,90 @@
+﻿using BossMod.QuestBattle;
+
+namespace BossMod.Endwalker.Quest.AsTheHeavensBurn.P3TerminusLacerator;
+
+public enum OID : uint
+{
+    Boss = 0x35EE,
+    Helper = 0x233C,
+    Vanquisher = 0x35EF,
+}
+
+public enum AID : uint
+{
+    _AutoAttack_Attack = 872, // 375A/3759/375B/375C/Boss->35F2/361A/3226/3604/3605/34A4/35F7, no cast, single-target
+    _Weaponskill_BlackStar = 27011, // Boss->self, 5.0s cast, single-target
+    _Weaponskill_BlackStar1 = 27012, // Helper->location, 6.0s cast, range 40 circle
+    _Weaponskill_DeadlyImpact = 27013, // Boss->self, 4.0s cast, single-target
+    _Weaponskill_DeadlyImpact1 = 27014, // Helper->location, 7.0s cast, range 10 circle
+    _Weaponskill_TheBlackDeath = 27010, // Boss->self, no cast, range 25 ?-degree cone
+    _Weaponskill_ForcefulImpact = 26239, // Vanquisher->location, 5.0s cast, range 7 circle
+    _Weaponskill_ForcefulImpactKB = 27030, // Helper->self, 5.6s cast, range 20 circle
+    _AutoAttack_ = 27028, // Vanquisher->35F7, no cast, single-target
+    _Weaponskill_WaveOfLoathing = 27032, // Vanquisher->self, 5.0s cast, range 40 circle
+    _Weaponskill_ForceOfLoathing = 27031, // Vanquisher->self, no cast, range 10 ?-degree cone
+    _Weaponskill_ = 27033, // Vanquisher->location, no cast, single-target
+    _Weaponskill_MutableLaws = 27039, // Vanquisher->self, 4.0s cast, single-target
+    _Weaponskill_MutableLaws1 = 27041, // Helper->location, 10.0s cast, range 6 circle
+    _Weaponskill_MutableLaws2 = 27040, // Helper->location, 10.0s cast, range 6 circle
+    _Weaponskill_AccursedTongue = 27037, // Vanquisher->self, 4.0s cast, single-target
+    _Weaponskill_AccursedTongue1 = 27038, // Helper->35F5/35FA/35F9/35F7, 5.0s cast, range 6 circle
+    _Weaponskill_Shock = 27035, // 35F0->self, 5.0s cast, range 10 circle
+    _Weaponskill_Depress = 27036, // 35EF->35FA, 5.0s cast, range 7 circle
+    _Weaponskill_ForcefulImpact2 = 27029, // 35EF->location, 5.0s cast, range 7 circle
+}
+
+class DeadlyImpact(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_DeadlyImpact1), 10, maxCasts: 6);
+class BlackStar(BossModule module) : Components.RaidwideCast(module, ActionID.MakeSpell(AID._Weaponskill_BlackStar1));
+
+class ForcefulImpact(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_ForcefulImpact), 7);
+class ForcefulImpactKB(BossModule module) : Components.KnockbackFromCastTarget(module, ActionID.MakeSpell(AID._Weaponskill_ForcefulImpactKB), 10, stopAtWall: true)
+{
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        if (Casters.FirstOrDefault() is Actor c)
+            hints.PredictedDamage.Add((WorldState.Party.WithSlot().Mask(), Module.CastFinishAt(c.CastInfo)));
+    }
+}
+class MutableLaws1(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_MutableLaws1), 15);
+class MutableLaws2(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_MutableLaws2), 6);
+class AccursedTongue(BossModule module) : Components.SpreadFromCastTargets(module, ActionID.MakeSpell(AID._Weaponskill_AccursedTongue1), 6);
+class ForcefulImpact2(BossModule module) : Components.LocationTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_ForcefulImpact2), 7);
+class Shock(BossModule module) : Components.SelfTargetedAOEs(module, ActionID.MakeSpell(AID._Weaponskill_Shock), new AOEShapeCircle(10), maxCasts: 6);
+class Depress(BossModule module) : Components.StackWithCastTargets(module, ActionID.MakeSpell(AID._Weaponskill_Depress), 7);
+
+class TerminusLaceratorStates : StateMachineBuilder
+{
+    private readonly TerminusLacerator _module;
+
+    public TerminusLaceratorStates(TerminusLacerator module) : base(module)
+    {
+        _module = module;
+
+        TrivialPhase()
+            .ActivateOnEnter<DeadlyImpact>()
+            .ActivateOnEnter<BlackStar>();
+        TrivialPhase(1)
+            .ActivateOnEnter<ForcefulImpact>()
+            .ActivateOnEnter<ForcefulImpactKB>()
+            .ActivateOnEnter<MutableLaws1>()
+            .ActivateOnEnter<MutableLaws2>()
+            .ActivateOnEnter<AccursedTongue>()
+            .ActivateOnEnter<ForcefulImpact2>()
+            .ActivateOnEnter<Shock>()
+            .ActivateOnEnter<Depress>()
+            .Raw.Update = () => _module.BossP2?.IsDeadOrDestroyed ?? false;
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 804, NameID = 10933)]
+public class TerminusLacerator(WorldState ws, Actor primary) : InstapullModule(ws, primary, new(-260.28f, 80.75f), new ArenaBoundsCircle(19.5f))
+{
+    public Actor? BossP2 => Enemies(OID.Vanquisher).FirstOrDefault();
+
+    protected override void DrawEnemies(int pcSlot, Actor pc)
+    {
+        Arena.Actor(PrimaryActor, ArenaColor.Enemy);
+        Arena.Actor(BossP2, ArenaColor.Enemy);
+    }
+}
+
