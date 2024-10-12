13fd380ddd518a49cd0aef443484f9747dad760e
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2024-10-12 00:49:33 -0400

    nice perf idiot

--- BossMod/Modules/Endwalker/Dungeon/D08Troia/D083Scarmiglione.cs
+++ BossMod/Modules/Endwalker/Dungeon/D08Troia/D083Scarmiglione.cs
@@ -30,26 +30,42 @@ class RottenRampage(BossModule module) : Components.LocationTargetedAOEs(module,
 class RottenRampagePlayer(BossModule module) : Components.SpreadFromCastTargets(module, ActionID.MakeSpell(AID._Weaponskill_RottenRampage2), 6);
 class VacuumWave(BossModule module) : Components.KnockbackFromCastTarget(module, ActionID.MakeSpell(AID._Weaponskill_VacuumWave), 30)
 {
+    private readonly List<(ulong ID, Angle Angle)> Walls = [];
+
+    public override void DrawArenaForeground(int pcSlot, Actor pc)
+    {
+        base.DrawArenaForeground(pcSlot, pc);
+        foreach (var (_, w) in Walls)
+        {
+            Arena.PathArcTo(Arena.Center, 20, (w - 6.5f.Degrees()).Rad, (w + 6.5f.Degrees()).Rad);
+            Arena.PathStroke(false, ArenaColor.Border);
+        }
+    }
+
+    public override void OnActorCreated(Actor actor)
+    {
+        if (actor.OID == 0x39C8)
+            Walls.Add((actor.InstanceID, Angle.FromDirection(actor.Position - Arena.Center)));
+    }
+
+    public override void OnActorDestroyed(Actor actor)
+    {
+        if (actor.OID == 0x39C8)
+            Walls.RemoveAll(w => w.ID == actor.InstanceID);
+    }
+
     public override IEnumerable<Source> Sources(int slot, Actor actor)
     {
         foreach (var c in Casters)
-            yield return new(c.Position, WallDist(actor.Position) > 0 ? 19 - (actor.Position - Module.Arena.Center).Length() : Distance, Module.CastFinishAt(c.CastInfo));
+            yield return new(c.Position, WallCheck(actor.Position) > 0 ? 19 - (actor.Position - Module.Arena.Center).Length() : Distance, Module.CastFinishAt(c.CastInfo));
     }
 
-    private float WallDist(WPos pos)
-    {
-        var zones = Module.Enemies(0x39C8).Select(w =>
-        {
-            var off = w.Position - Arena.Center;
-            return ShapeDistance.InvertedCone(Arena.Center, off.Length(), Angle.FromDirection(off), 6.5f.Degrees());
-        }).ToList();
-        return zones.Max(e => e(pos));
-    }
+    private float WallCheck(WPos pos) => Walls.Max(w => pos.InCone(Arena.Center, w.Angle, 6.5f.Degrees()) ? 1 : 0);
 
     public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
     {
         if (Casters.FirstOrDefault() is Actor c)
-            hints.AddForbiddenZone(WallDist, Module.CastFinishAt(c.CastInfo));
+            hints.AddForbiddenZone(p => WallCheck(p) - 1, Module.CastFinishAt(c.CastInfo));
     }
 }
 
@@ -71,21 +87,6 @@ class ScarmiglioneStates : StateMachineBuilder
 [ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 869, NameID = 11372)]
 public class Scarmiglione(WorldState ws, Actor primary) : BossModule(ws, primary, new(-35, -298), new ArenaBoundsCircle(25))
 {
-    protected override void DrawArenaForeground(int pcSlot, Actor pc)
-    {
-        foreach (var w in Enemies(0x39C8))
-        {
-            // Arena.Actor(w, ArenaColor.Object, true);
-            // Arena.AddRect(w.Position, w.Rotation.ToDirection(), 1, 0, 2, ArenaColor.Object);
-            var off = w.Position - Arena.Center;
-            Arena.PathArcTo(Arena.Center, off.Length() - 1.5f, Angle.FromDirection(off).Rad - 6.5f.Degrees().Rad, Angle.FromDirection(off).Rad + 6.5f.Degrees().Rad);
-            Arena.PathStroke(false, ArenaColor.Border);
-        }
-    }
-
-    protected override void DrawArenaBackground(int pcSlot, Actor pc)
-    {
-        Arena.ZoneDonut(Arena.Center, 21, 25, ArenaColor.AOE);
-    }
+    protected override void DrawArenaBackground(int pcSlot, Actor pc) => Arena.ZoneDonut(Arena.Center, 21, 25, ArenaColor.AOE);
 }
 
